c096e316af76645950e7e4e4c2055572fee4f47c
Moojun <mgim32137@gmail.com>
2023-07-20 18:13:34 +0900

    initial commit

---
+++ src/test/java/org/ai/roboadvisor/RoboAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ai.roboadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
