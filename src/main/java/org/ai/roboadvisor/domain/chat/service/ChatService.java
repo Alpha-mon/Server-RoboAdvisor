e4d426a4b080708db3631ee5fefcbcb5c1179fda
Moojun <mgim32137@gmail.com>
2023-07-31 14:33:45 +0900

    #1 - refactor: Use WebClient

--- src/main/java/org/ai/roboadvisor/domain/chat/service/ChatService.java
+++ src/main/java/org/ai/roboadvisor/domain/chat/service/ChatService.java
@@ -8,10 +8,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.ai.roboadvisor.domain.chat.dto.Message;
 import org.ai.roboadvisor.domain.chat.dto.request.ChatGptRequest;
 import org.ai.roboadvisor.domain.chat.dto.response.ChatGptResponse;
+import org.ai.roboadvisor.domain.chat.entity.Chat;
 import org.ai.roboadvisor.global.exception.CustomException;
 import org.ai.roboadvisor.global.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.net.URI;
@@ -19,7 +26,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -38,6 +47,12 @@ public class ChatService {
     @Value("${openai.api.secret-key}")
     private String OPEN_AI_SECRET_KEY;
 
+    @Transactional
+    public List<Chat> getChatList() {
+
+        return null;
+    }
+
     public Message getMessageFromGpt(String message) {
         ChatGptRequest chatGptRequest = ChatGptRequest
                 .builder()
@@ -48,10 +63,14 @@ public class ChatService {
                         .build()))
                 .build();
 
-        String response = sendRequestToGpt(chatGptRequest);
+        String result = sendRequestToGpt(chatGptRequest).block();
+
+
+        // 여기서 DB에 메시지 저장
+
 
         try {
-            ChatGptResponse chatGptResponse = objectMapper.readValue(response, ChatGptResponse.class);
+            ChatGptResponse chatGptResponse = objectMapper.readValue(result, ChatGptResponse.class);
 
             return Message.builder()
                     .role(chatGptResponse.getChoices().get(0).getMessage().getRole())
@@ -63,25 +82,19 @@ public class ChatService {
         }
     }
 
-    public String sendRequestToGpt(ChatGptRequest gptRequest) {
+    public Mono<String> sendRequestToGpt(ChatGptRequest gptRequest) {
         String jsonToStr = gson.toJson(gptRequest);
-        String response;
 
-        try {
-            HttpClient client = HttpClient.newHttpClient();
+        WebClient webClient = WebClient.builder()
+                .baseUrl(OPEN_AI_URL)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + OPEN_AI_SECRET_KEY)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(OPEN_AI_URL))
-                    .setHeader("Content-Type", "application/json")
-                    .setHeader("Authorization", "Bearer " + OPEN_AI_SECRET_KEY)
-                    .POST(BodyPublishers.ofString(jsonToStr))
-                    .build();
-            response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-        } catch (IOException | InterruptedException e) {
-            log.error("[Error in ChatService -> sendRequestToGpt] ", e);
-            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
-        }
-        return response;
+        return webClient.post()
+                .body(BodyInserters.fromValue(jsonToStr))
+                .retrieve()
+                .bodyToMono(String.class);  // String.class can be replaced with a class if you have a model for the response
     }
 
 }
