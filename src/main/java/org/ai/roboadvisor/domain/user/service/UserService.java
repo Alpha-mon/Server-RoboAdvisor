d332ad75b047db2833117e8ab4c61fbe8573ef03
Moojun <mgim32137@gmail.com>
2023-09-21 01:15:26 +0900

    fix build error

--- src/main/java/org/ai/roboadvisor/domain/user/service/UserService.java
+++ src/main/java/org/ai/roboadvisor/domain/user/service/UserService.java
@@ -2,10 +2,10 @@ package org.ai.roboadvisor.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ai.roboadvisor.domain.tendency.entity.Tendency;
 import org.ai.roboadvisor.domain.user.dto.request.SignInRequest;
 import org.ai.roboadvisor.domain.user.dto.request.SignUpRequest;
 import org.ai.roboadvisor.domain.user.dto.response.SignInResponse;
-import org.ai.roboadvisor.domain.user.entity.Tendency;
 import org.ai.roboadvisor.domain.user.entity.User;
 import org.ai.roboadvisor.domain.user.repository.UserRepository;
 import org.ai.roboadvisor.global.exception.CustomException;
