ab6dad62974a16739b2e958105c537cf0b52a568
Moojun <mgim32137@gmail.com>
2023-09-25 05:31:27 +0900

    #14 - chore: Create swagger in BoardController

--- src/main/java/org/ai/roboadvisor/domain/community/controller/BoardController.java
+++ src/main/java/org/ai/roboadvisor/domain/community/controller/BoardController.java
@@ -1,13 +1,16 @@
 package org.ai.roboadvisor.domain.community.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ai.roboadvisor.domain.community.dto.response.BoardResponse;
 import org.ai.roboadvisor.domain.community.service.BoardService;
+import org.ai.roboadvisor.domain.community.swagger_annotation.board.getAllPostsByType.*;
 import org.ai.roboadvisor.domain.tendency.entity.Tendency;
 import org.ai.roboadvisor.global.common.dto.SuccessApiResponse;
 import org.ai.roboadvisor.global.exception.SuccessCode;
+import org.ai.roboadvisor.global.swagger_annotation.ApiResponse_Internal_Server_Error;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -22,7 +25,7 @@ import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@Tag(name = "community] board", description = "게시글 불러오기 및 조회 API")
+@Tag(name = "community] board", description = "게시글 전체 불러오기 API")
 @RestController
 @RequestMapping("/api/community/board")
 public class BoardController {
@@ -31,6 +34,10 @@ public class BoardController {
 
     private final int PAGE_SIZE = 10;
 
+    @Operation(summary = "게시글 전체 조회", description = "게시글 전체 불러오기 API")
+    @getAllPostsByType_OK
+    @getAllPostsByType_BAD_REQUEST
+    @ApiResponse_Internal_Server_Error
     @GetMapping()
     public ResponseEntity<SuccessApiResponse<List<BoardResponse>>> getAllPostsByType(
             @RequestParam("tendency") Tendency tendency, @PageableDefault(size = PAGE_SIZE, sort = {"id"},
---
+++ src/main/java/org/ai/roboadvisor/domain/community/swagger_annotation/board/getAllPostsByType/getAllPostsByType_OK.java
@@ -0,0 +1,118 @@
+package org.ai.roboadvisor.domain.community.swagger_annotation.board.getAllPostsByType;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.ai.roboadvisor.global.common.dto.SuccessApiResponse;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ApiResponse(responseCode = "200", description = """
+        게시글 전체 불러오기
+                
+        Pagination을 사용하여, page=0, 1, 2, ... 숫자를 증가시킴에 따라 연속해서 게시글을 가져올 수 있다.
+                
+        하나의 page 마다 10개의 게시글을 가져오도록 구현되어 있으며, 이는 변경이 가능하다.
+                
+        요청 예시는 다음과 같다: [api url]/api/community/board?tendency=LION&page=0
+                
+        data Array 내에서 각 인덱스가 하나의 게시글 정보이다.
+                
+        id: 게시글 id, tendency: 투자 성향, nickname: 작성자 닉네임, content: 게시글 내용,
+                
+        createdDateTime: 게시글 작성 시간, viewCount: 조회수, commentCount: 게시글 내의 댓글 개수
+        """,
+        content = @Content(schema = @Schema(implementation = SuccessApiResponse.class),
+                examples =
+                @ExampleObject(name = "example",
+                        description = "정상 응답",
+                        value = """
+                                   {
+                                       "code": 200,
+                                       "message": "게시글을 불러오는데 성공하셨습니다",
+                                       "data": [
+                                           {
+                                               "id": 1,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요3333",
+                                               "createdDateTime": "2023-09-21 01:06:20",
+                                               "viewCount": 9,
+                                               "commentCount": 2
+                                           },
+                                           {
+                                               "id": 3,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:14",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 4,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:15",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 5,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:15",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 6,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:16",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 7,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:16",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 8,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:17",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           },
+                                           {
+                                               "id": 9,
+                                               "tendency": "LION",
+                                               "nickname": "testUser",
+                                               "content": "안녕하세요",
+                                               "createdDateTime": "2023-09-23 14:44:18",
+                                               "viewCount": 0,
+                                               "commentCount": 0
+                                           }
+                                       ]
+                                   }
+                                """
+                )
+
+        ))
+public @interface getAllPostsByType_OK {
+}
+
